0a222c3b9997b60c652026425969e0f89332be08
César Pinto Castillo <cesar@jagcesar.se>
2015-12-25 02:01:18 +0100

    Adds Postgres

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var koa = require('koa');
-var app = koa();
+var koa = require('koa'),
+	route = require('koa-route'),
+	app = module.exports = koa(),
+	pg = require('pg');
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+app.use(route.get('/db', db));
+
+function *db() {
+
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
+
+		client
+		.query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    })
+  });
+
+  this.body = 'db';
+}
 
 app.listen((process.env.PORT || 4000));
