bc5f981986ab8dbd44e5c364862ec115f9b9627e
César Pinto Castillo <cesar@jagcesar.se>
2015-12-26 20:51:54 +0100

    Adds koa-bouncer to validate params

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var koa = require('koa'),
 	route = require('koa-route'),
 	app = module.exports = koa(),
-	pg = require('pg');
+	pg = require('pg'),
+	bouncer = require('koa-bouncer');
 
+app.use(bouncer.middleware());
+
+app.use(route.get('/', index));
 app.use(route.get('/db', db));
+app.use(route.get('/asd', asd));
+
+function *index() {
+	this.body = 'Hello world';
+}
 
 function *db() {
-
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		if (err) throw err;
 		console.log('Connected to postgres! Getting schemas...');
@@ -19,6 +27,15 @@ function *db() {
   });
 
   this.body = 'db';
+}
+
+function *asd() {
+	this.validateQuery('asd')
+		.required('asd required')
+    .isString()
+    .trim();
+
+    this.body = 'valid';
 }
 
 app.listen((process.env.PORT || 4000));
