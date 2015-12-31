7bb6b767903812ad039de80bd4cebd52e6825ec6
César Pinto Castillo <cesar@jagcesar.se>
2015-12-31 01:12:33 +0100

    Fixes so we only have space indentation

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var koa = require('koa'),
-	route = require('koa-route'),
-	app = module.exports = koa(),
-	koaPg = require('koa-pg'),
-	bouncer = require('koa-bouncer'),
-	jwt = require('koa-jwt'),
-	uuid = require('uuid'),
-	json = require('koa-json');
+  route = require('koa-route'),
+  app = module.exports = koa(),
+  koaPg = require('koa-pg'),
+  bouncer = require('koa-bouncer'),
+  jwt = require('koa-jwt'),
+  uuid = require('uuid'),
+  json = require('koa-json');
 
-app.use(koaPg(process.env.DATABASE_URL))
+app.use(koaPg(process.env.DATABASE_URL));
 app.use(bouncer.middleware());
 app.use(json({ pretty: false, param: 'pretty' }));
 
@@ -17,32 +17,33 @@ app.use(route.get('/validate', validate));
 app.use(route.get('/register', register));
 
 function *index() {
-	this.body = {'message': 'Hello world'};
+  console.log(this.req);
+  this.body = {'message': 'Hello world'};
 }
 
 function *initDB() {
-	yield this.pg.db.client.query_('CREATE EXTENSION postgis;');
-	yield this.pg.db.client.query_('CREATE TABLE "users" ("id" SERIAL, "uuid" UUID NOT NULL, PRIMARY KEY ("id"));');
-	yield this.pg.db.client.query_('CREATE TABLE "check_ins" ("id" serial, "users.id" serial, PRIMARY KEY ("id"), FOREIGN KEY ("users.id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE);');
-	yield this.pg.db.client.query_('SELECT AddGeometryColumn(\'check_ins\', \'location\', 4326, \'POINT\', 2);');
-	this.body = '1'
+  yield this.pg.db.client.query_('CREATE EXTENSION postgis;');
+  yield this.pg.db.client.query_('CREATE TABLE "users" ("id" SERIAL, "uuid" UUID NOT NULL, PRIMARY KEY ("id"));');
+  yield this.pg.db.client.query_('CREATE TABLE "check_ins" ("id" serial, "users.id" serial, PRIMARY KEY ("id"), FOREIGN KEY ("users.id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE);');
+  yield this.pg.db.client.query_('SELECT AddGeometryColumn(\'check_ins\', \'location\', 4326, \'POINT\', 2);');
+  this.body = '1'
 }
 
 function *validate() {
-	this.validateQuery('argument')
-		.required('argument required')
+  this.validateQuery('argument')
+    .required('argument required')
     .isString()
     .trim();
 
-    this.body = {'message': 'Valid query'};
+  this.body = {'message': 'Valid query'};
 }
 
 function *register() {
-	var uuidString = uuid.v4();
-	var query = 'INSERT INTO users (uuid) VALUES (\'' + uuidString + '\');';
-	var result = yield this.pg.db.client.query_(query);
-	var token = jwt.sign({ uuid: uuidString }, process.env.JWT_SECRET);
-	this.body = {'jwt': token};
+  var uuidString = uuid.v4();
+  var query = 'INSERT INTO users (uuid) VALUES (\'' + uuidString + '\');';
+  var result = yield this.pg.db.client.query_(query);
+  var token = jwt.sign({ uuid: uuidString }, process.env.JWT_SECRET);
+  this.body = {'jwt': token};
 }
 
 app.use(jwt({secret: process.env.JWT_SECRET}));
@@ -51,24 +52,24 @@ app.use(route.get('/protected', protected));
 app.use(route.get('/checkIn', checkIn));
 
 function *protected() {
-	this.body = this.state.user;
+  this.body = this.state.user;
 }
 
 function *checkIn() {
-	this.validateQuery('lat')
-		.required('Latitude required');
+  this.validateQuery('lat')
+    .required('Latitude required');
 
-	this.validateQuery('lon')
-		.required('Longitude required');
+  this.validateQuery('lon')
+    .required('Longitude required');
 
-	var uuid = this.state.user['uuid'];
-	var query = 'INSERT INTO check_ins ("location", "users.id") VALUES (ST_GeomFromText(\'POINT(' + this.vals.lat + ' ' + this.vals.lon + ')\', 4326), (SELECT id FROM users WHERE uuid=\'' + uuid + '\'));';
-	var result = yield this.pg.db.client.query_(query);
-	this.body = '';
+  var uuid = this.state.user['uuid'];
+  var query = 'INSERT INTO check_ins ("location", "users.id") VALUES (ST_GeomFromText(\'POINT(' + this.vals.lat + ' ' + this.vals.lon + ')\', 4326), (SELECT id FROM users WHERE uuid=\'' + uuid + '\'));';
+  var result = yield this.pg.db.client.query_(query);
+  this.body = '';
 }
 
 function *distance() {
-	var query = 'SELECT location FROM check_ins WHERE ST_Distance_Sphere(location,ST_GeomFromText(\'POINT(59.3354419 18.0577941)\', 4326)) < 500;';
+  var query = 'SELECT location FROM check_ins WHERE ST_Distance_Sphere(location,ST_GeomFromText(\'POINT(59.3354419 18.0577941)\', 4326)) < 500;';
 }
 
 app.listen(process.env.PORT);
