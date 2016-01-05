66186e6ee7c8cced3ff2675d6e2959649a65283e
César Pinto Castillo <cesar@jagcesar.se>
2016-01-05 23:15:55 +0100

    Adds a GUID to places

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ function *index() {
 
 function *initDB() {
   yield this.pg.db.client.query_('CREATE EXTENSION postgis;');
-  yield this.pg.db.client.query_('CREATE TABLE "users" ("id" SERIAL, "uuid" UUID NOT NULL, PRIMARY KEY ("id"));');
-  yield this.pg.db.client.query_('CREATE TABLE "places" ("id" serial, "name" text, PRIMARY KEY ("id"));');
+  yield this.pg.db.client.query_('CREATE TABLE "users" ("id" SERIAL, "uuid" UUID NOT NULL, PRIMARY KEY ("id"), UNIQUE ("uuid"));');
+  yield this.pg.db.client.query_('CREATE TABLE "places" ("id" serial, "name" text, "guid" UUID NOT NULL, PRIMARY KEY ("id"), UNIQUE ("guid"));');
   yield this.pg.db.client.query_('SELECT AddGeometryColumn(\'places\', \'coordinate\', 4326, \'POINT\', 2);');
   yield this.pg.db.client.query_('CREATE TABLE "check_ins" ("id" serial, "users.id" serial, "places.id" serial, PRIMARY KEY ("id"), FOREIGN KEY ("users.id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY ("places.id") REFERENCES "places"("id") ON DELETE CASCADE ON UPDATE CASCADE);');
 
-  // Let's populate the places table
-  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate") VALUES (\'Apple HQ\', ST_GeomFromText(\'POINT(37.3317115 -122.0323722)\', 4326));');
-  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate") VALUES (\'Facebook HQ\', ST_GeomFromText(\'POINT(37.4833149 -122.1517118)\', 4326));');
-  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate") VALUES (\'Google HQ\', ST_GeomFromText(\'POINT(37.4219999 -122.0862462)\', 4326));');
-  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate") VALUES (\'Twitter HQ\', ST_GeomFromText(\'POINT(37.776692 -122.4189706)\', 4326));');
+  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate", "guid") VALUES (\'Apple HQ\', ST_GeomFromText(\'POINT(37.3317115 -122.0323722)\', 4326), \'' + uuid.v4() + '\');');
+  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate", "guid") VALUES (\'Facebook HQ\', ST_GeomFromText(\'POINT(37.4833149 -122.1517118)\', 4326), \'' + uuid.v4() + '\');');
+  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate", "guid") VALUES (\'Google HQ\', ST_GeomFromText(\'POINT(37.4219999 -122.0862462)\', 4326), \'' + uuid.v4() + '\');');
+  yield this.pg.db.client.query_('INSERT INTO places ("name", "coordinate", "guid") VALUES (\'Twitter HQ\', ST_GeomFromText(\'POINT(37.776692 -122.4189706)\', 4326), \'' + uuid.v4() + '\');');
 
   this.body = '1';
 }
@@ -49,25 +48,26 @@ app.use(route.get('/checkIn', checkIn));
 app.use(route.get('/locations', locations));
 
 function *checkIn() {
-  this.validateQuery('lat')
-    .required('Latitude required');
+  this.validateQuery('locationId')
+    .required('Location id required')
+    .isUuid('v4', 'Location id must be a valid UUID v4');
 
   this.validateQuery('lon')
     .required('Longitude required');
 
   var uuid = this.state.user['uuid'];
-  var query = 'INSERT INTO check_ins ("location", "users.id") VALUES (ST_GeomFromText(\'POINT(' + this.vals.lat + ' ' + this.vals.lon + ')\', 4326), (SELECT id FROM users WHERE uuid=\'' + uuid + '\'));';
+  var query = 'INSERT INTO check_ins ("places.id", "users.id") VALUES (' + this.vals.locationId + ', (SELECT id FROM users WHERE uuid=\'' + uuid + '\'));';
   var result = yield this.pg.db.client.query_(query);
   this.body = '';
 }
 
 function *locations() {
-  result = yield this.pg.db.client.query_('SELECT id, name FROM places;');
+  result = yield this.pg.db.client.query_('SELECT uuid, name FROM places;');
   this.body = result.rows;
 }
 
 function *distance() {
-  var query = 'SELECT location FROM check_ins WHERE ST_Distance_Sphere(location,ST_GeomFromText(\'POINT(59.3354419 18.0577941)\', 4326)) < 500;';
+  var query = 'SELECT coordinate FROM places WHERE ST_Distance_Sphere(coordinate, ST_GeomFromText(\'POINT(59.3354419 18.0577941)\', 4326)) < 500;';
 }
 
 app.listen(process.env.PORT);
